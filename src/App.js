960ebb8b689e663ad38edc6b7386cf678925f19e
Christopher <leechrs1@gmail.com>
2023-03-05 22:41:33 -0600

    worked on adding a new fact. Submit button works and a new facts is added

--- src/App.js
+++ src/App.js
@@ -42,18 +42,20 @@ const initialFacts = [
 
 function App() {
   // Define state variable
-  const [showForm, setShowForm] = useState(false)
+  const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
     <Header showForm = {showForm}setShowForm = {setShowForm}/>
     
     {/* Use state variable */}
-    {showForm ? <NewFactForm /> : null}
+    {showForm ? <NewFactForm setFacts={setFacts} 
+    setShowForm={setShowForm}/> : null}
 
     <main className = "main">
      <CategoryFilter />
-     <FactList />
+     <FactList facts={facts}/>
     </main>
     </>
   )
@@ -75,14 +77,49 @@ function Header({ showForm, setShowForm }) {
         </header>
 )};
 
-function NewFactForm() {
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText]=useState("");
-  const [source, setSource]=useState("");
+  const [source, setSource]=useState("http://example.com");
   const [category, setCategory]=useState("");
-  const textLength = text.length
+  const textLength = text.length;
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(text, source, category)
+    // TODO Prevent browser reload
+
+    // TODO Check if data is valid. If valid create a new fact
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {      
+    // TODO Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 1000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear()
+      }
+    // TODO Add new fact to the UI: add the fact to state
+      setFacts((facts) => [newFact, ...facts])
+    // TODO Reset input fields
+      setText('');
+      setSource('');
+      setCategory('');
+    // TODO Close entire form
+      setShowForm(false);
+    };
   };
 
   return <form className="fact-form" onSubmit={handleSubmit}>
@@ -115,20 +152,17 @@ function CategoryFilter() {
     <li className="category"><button className="btn btn-all-categories">All</button></li>
       {CATEGORIES.map((cat) => 
       <li key={cat.name} className="category">
-        <button class="btn btn-category" style={{ backgroundColor: cat.color }}>{cat.name}</button></li>)}
+        <button className="btn btn-category" style={{ backgroundColor: cat.color }}>{cat.name}</button></li>)}
     </ul>
   </aside>
 };
 
-function FactList() {
-  const facts = initialFacts;
-
+function FactList({ facts }) {  
   return <section><ul className="facts-list">{
     facts.map((fact) => <Fact key={fact.id} fact={fact}/>)}
     </ul>
     <p>There are {facts.length} facts in the database. Add your own!</p>
     </section>
-  
 };
 
 function Fact({ fact }) {
@@ -136,10 +170,10 @@ function Fact({ fact }) {
   <li className="fact">
   <p>
   {fact.text}
-  <a class="source" href={fact.source} target="_blank">(source)</a>
+  <a className="source" href={fact.source} >(source)</a>
   </p>
-  <span class="tag" style={{backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color}}>{fact.category}</span>
-  <div class="vote-buttons">
+  <span className="tag" style={{backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color}}>{fact.category}</span>
+  <div className="vote-buttons">
       <button>👍 {fact.votesInteresting}</button>
       <button>🤯 {fact.votesMindblowing}</button>
       <button>⛔️ {fact.votesFalse}</button>
